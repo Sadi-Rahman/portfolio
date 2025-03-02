af2c8f85e189e58e2a8f05180690272e983070e1
Md. Abu Bakar Siddique Sadi <sadiakash1309@gmail.com>
2025-03-02 23:21:56 +0600

    Basic structure done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // import "./App.css";
-import Hero from "./components/hero";
+import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 import Footer from "./components/Footer";
 import About from "./components/About";
